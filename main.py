2668bf7abaf8adb8cad5f11752a12d50e3a23f39
Name <user@example.com>
2023-10-09 19:19:48 +0200

    Program get_data function

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import plotly.express as px
+from backend import get_data
 
 st.title("Weather Forecast for the Next Days")
 place = st.text_input("Place:")
@@ -8,8 +9,9 @@ days = st.slider("Forecast Days", min_value=1, max_value=5,
 option = st.selectbox("Select data to view", ("Temperature", "Sky"))
 st.subheader(f"{option} for the next {days} days in {place}")
 
-dates = ["2023-10-09", "2023-10-10", "2023-10-11"]
-temperatures = [12, 20, 15]
+
+data = get_data(place, days, option)
+
 figure = px.line(x=dates, y=temperatures,
                  labels={"x": "dates", "y": "temperatures in C"})
 st.plotly_chart(figure)
