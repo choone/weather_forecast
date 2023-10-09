a69287a8c136a3d2225371634a5bc68c398cdbc5
Name <user@example.com>
2023-10-09 21:39:05 +0200

    Final commit

--- backend.py
+++ backend.py
@@ -1,6 +1,6 @@
 import requests
 
-def get_data(place,forecast_days=None, kind=None):
+def get_data(place,forecast_days=None):
     API_KEY ="d36403e6f02debcffddb30367d080f69"
     url = f"http://api.openweathermap.org/data/2.5/forecast?q={place}&appid={API_KEY}"
     response = requests.get(url)
@@ -8,12 +8,8 @@ def get_data(place,forecast_days=None, kind=None):
     filtered_data = data["list"]
     nr_values = 8*forecast_days
     filtered_data = filtered_data[:nr_values]
-    if kind == "Temperature":
-       filtered_data = [dict["main"]["temp"] for dict in filtered_data]
-    if kind == "Sky":
-        filtered_data = [dict["weather"][0]["main"] for dict in filtered_data]
     return filtered_data
 
 
 if __name__=="__main__":
-    print(get_data(place="Tokyo", forecast_days=2, kind="Temperature"))
+    print(get_data(place="Tokyo", forecast_days=2))
--- main.py
+++ main.py
@@ -9,9 +9,19 @@ days = st.slider("Forecast Days", min_value=1, max_value=5,
 option = st.selectbox("Select data to view", ("Temperature", "Sky"))
 st.subheader(f"{option} for the next {days} days in {place}")
 
+if place:
+    filtered_data = get_data(place, days)
 
-data = get_data(place, days, option)
+    if option == "Temperature":
+        temperatures = [dict["main"]["temp"]-273.15 for dict in filtered_data]
+        dates = [dict["dt_txt"] for dict in filtered_data]
+        figure = px.line(x=dates, y=temperatures,
+                     labels={"x": "dates", "y": "temperatures (C)"})
+        st.plotly_chart(figure)
 
-figure = px.line(x=dates, y=temperatures,
-                 labels={"x": "dates", "y": "temperatures in C"})
-st.plotly_chart(figure)
+    if option == "Sky":
+        images = {"Clear": "images/clear.png", "Clouds": "images/cloud.png",
+                  "Rain": "images/rain.png", "Snow": "images/snow.png"}
+        sky_conditions = [dict["weather"][0]["main"] for dict in filtered_data]
+        image_paths = [images[condition] for condition in sky_conditions]
+        st.image(image_paths, width=80)
